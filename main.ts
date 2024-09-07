951779d69cc60487dea152663c2e5454bcf720c1
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-07 17:37:45 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,35 +1,47 @@
+function zeitEingeben (zeichenCode: number) {
+    if (pins.between(zeichenCode, 48, 57)) {
+        ziffer = zeichenCode - 48
+        if (zahl < 10) {
+            zahl = zahl * 10 + ziffer
+        } else {
+            zahl = ziffer
+        }
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 13, 15, zahl, lcd16x2rgb.eAlign.right)
+        return true
+    } else {
+        return false
+    }
+}
 pins.onKeyboardEvent(function (zeichenCode, zeichenText, isASCII) {
     lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 9, 9, lcd16x2rgb.lcd16x2_text(zeichenText))
     if (ledButton) {
-        if ((zeichenText == "*" || zeichenCode == 180) && zeitIndex > 0) {
-            zeitSpeichern()
-            zeitIndex += -1
-        } else if ((zeichenText == "#" || zeichenCode == 183) && zeitIndex < 6) {
-            zeitSpeichern()
-            zeitIndex += 1
-        } else if (zeichenCode == 27) {
-            zeitNeu = false
-        }
+        zeitWahl(zeichenCode, zeichenText)
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 14, 15, zeitRegister[zeitIndex])
-        zeitWert = rtcpcf85063tp.getByte(zeitIndex, rtcpcf85063tp.eFormat.DEC)
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 11, 12, zeitWert)
-        if (pins.between(zeichenCode, 48, 57)) {
-            ziffer = zeichenCode - 48
-            if (zahl < 10) {
-                zahl = zahl * 10 + ziffer
-            } else {
-                zahl = ziffer
-            }
-            zeitNeu = true
-            lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 13, 15, zahl, lcd16x2rgb.eAlign.right)
-        }
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 11, 12, rtcpcf85063tp.getByte(zeitIndex, rtcpcf85063tp.eFormat.DEC))
+        zeitNeu = zeitEingeben(zeichenCode)
+    } else {
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 14, 15, lcd16x2rgb.lcd16x2_text(""))
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 11, 15, lcd16x2rgb.lcd16x2_text(""))
     }
 })
+function zeitWahl (zeichenCode: number, zeichenText: string) {
+    if ((zeichenText == "*" || zeichenCode == 180) && zeitIndex > 0) {
+        zeitSpeichern()
+        zeitIndex += -1
+    } else if ((zeichenText == "#" || zeichenCode == 183) && zeitIndex < 6) {
+        zeitSpeichern()
+        zeitIndex += 1
+    } else if (zeichenCode == 27) {
+        pins.comment(pins.pins_text("ESC Taste"))
+        zeitNeu = false
+    }
+}
 function zeitSpeichern () {
     if (zeitNeu) {
         rtcpcf85063tp.writeDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), zeitIndex, [zahl])
         zeitNeu = false
         zahl = 0
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 13, 15, zahl, lcd16x2rgb.eAlign.right)
     }
 }
 pins.onPulsed(DigitalPin.C17, PulseValue.Low, function () {
@@ -37,9 +49,12 @@ pins.onPulsed(DigitalPin.C17, PulseValue.Low, function () {
         ledButton = !(ledButton)
     }
     pins.pinDigitalWrite(pins.pins_eDigitalPins(pins.eDigitalPins.C16), !(ledButton))
+    if (!(ledButton)) {
+        zeitNeu = false
+        zahl = 0
+    }
 })
 let ziffer = 0
-let zeitWert = 0
 let zeitIndex = 0
 let zeitNeu = false
 let zahl = 0
@@ -60,6 +75,7 @@ zeitRegister = [
 pins.pinDigitalWrite(pins.pins_eDigitalPins(pins.eDigitalPins.C16), !(ledButton))
 zahl = 0
 zeitNeu = false
+zeitIndex = 6
 loops.everyInterval(200, function () {
     rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
     if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute))) {
