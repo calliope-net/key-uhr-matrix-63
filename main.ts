249a8be71adf97b33130fe4a32eb7545a3303f6d
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-09-07 17:56:42 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,9 +19,6 @@ pins.onKeyboardEvent(function (zeichenCode, zeichenText, isASCII) {
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 14, 15, zeitRegister[zeitIndex])
         lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 11, 12, rtcpcf85063tp.getByte(zeitIndex, rtcpcf85063tp.eFormat.DEC))
         zeitNeu = zeitEingeben(zeichenCode)
-    } else {
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 14, 15, lcd16x2rgb.lcd16x2_text(""))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 11, 15, lcd16x2rgb.lcd16x2_text(""))
     }
 })
 function zeitWahl (zeichenCode: number, zeichenText: string) {
@@ -39,10 +36,17 @@ function zeitWahl (zeichenCode: number, zeichenText: string) {
 function zeitSpeichern () {
     if (zeitNeu) {
         rtcpcf85063tp.writeDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51), zeitIndex, [zahl])
-        zeitNeu = false
-        zahl = 0
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 13, 15, zahl, lcd16x2rgb.eAlign.right)
     }
+}
+function anzeigenSekunde () {
+    rtcpcf85063tp.anzeige25LED(rtcpcf85063tp.e25LED.Zeit)
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, lcd16x2rgb.lcd16x2_text(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit)))
+}
+function anzeigenMinute () {
+    matrix.clearMatrix()
+    matrix.writeClock(64, 63, 63, rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.DEC), rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC))
+    matrix.displayMatrix()
+    lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 12, rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.mit, rtcpcf85063tp.ePart.mit))
 }
 pins.onPulsed(DigitalPin.C17, PulseValue.Low, function () {
     if (pins.pulseDuration() > 10000) {
@@ -50,10 +54,10 @@ pins.onPulsed(DigitalPin.C17, PulseValue.Low, function () {
     }
     pins.pinDigitalWrite(pins.pins_eDigitalPins(pins.eDigitalPins.C16), !(ledButton))
     if (!(ledButton)) {
-        zeitNeu = false
-        zahl = 0
+        eingabeEnde = true
     }
 })
+let eingabeEnde = false
 let ziffer = 0
 let zeitIndex = 0
 let zeitNeu = false
@@ -76,17 +80,19 @@ pins.pinDigitalWrite(pins.pins_eDigitalPins(pins.eDigitalPins.C16), !(ledButton)
 zahl = 0
 zeitNeu = false
 zeitIndex = 6
+anzeigenMinute()
 loops.everyInterval(200, function () {
     rtcpcf85063tp.readDateTime(rtcpcf85063tp.rtcpcf85063tp_eADDR(rtcpcf85063tp.eADDR.RTC_x51))
     if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute))) {
-        matrix.clearMatrix()
-        matrix.writeClock(64, 63, 63, rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Stunde), rtcpcf85063tp.eFormat.DEC), rtcpcf85063tp.getByte(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Minute), rtcpcf85063tp.eFormat.DEC))
-        matrix.displayMatrix()
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 12, rtcpcf85063tp.getDate(rtcpcf85063tp.ePart.mit, rtcpcf85063tp.ePart.mit))
-    }
-    if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde))) {
-        rtcpcf85063tp.anzeige25LED(rtcpcf85063tp.e25LED.Zeit)
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 0, 7, lcd16x2rgb.lcd16x2_text(rtcpcf85063tp.getTime(rtcpcf85063tp.ePart.mit)))
+        anzeigenMinute()
+    } else if (rtcpcf85063tp.isChanged(rtcpcf85063tp.rtcpcf85063tp_eRegister(rtcpcf85063tp.eRegister.Sekunde))) {
+        anzeigenSekunde()
+    } else if (eingabeEnde) {
+        eingabeEnde = false
+        zeitNeu = false
+        zahl = 0
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 14, 15, lcd16x2rgb.lcd16x2_text(""))
+        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 1, 9, 15, lcd16x2rgb.lcd16x2_text(""))
     }
     pins.raiseKeyboardEvent(true)
     pins.raiseKeypadEvent(true)
